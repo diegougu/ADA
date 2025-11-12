f42e013166bfcebb21f4d99db4cf0a378d88bc64
diegougu <128000056+diegougu@users.noreply.github.com>
2025-11-12 13:01:14 -0500

    Create quicksort.cpp

---
+++ quicksort.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int pivot(vector<int>& vc, int ini, int fin) {
+	int pivotval = vc[ini];
+	int i = ini + 1;
+	int j = fin;
+	while (true) {
+		while (i <= fin && vc[i] <= pivotval) {
+			i++;
+		}
+		while (j >= ini && vc[j] > pivotval) {
+			j--;
+		}
+		if (i >= j) break;
+		swap(vc[i], vc[j]);
+	}
+	swap(vc[ini], vc[j]);
+	return j;
+}
+
+void quicksort(vector<int>& vc, int ini, int fin) {
+	if (ini >= fin) {
+		return;
+	}
+	int pos = pivot(vc, ini, fin);
+	quicksort(vc, ini, pos - 1);
+	quicksort(vc, pos + 1, fin);
+}
+
+int main() {
+	vector<int> v = { 9, 3, 7, 1, 6, 5, 2, 8, 4 };
+
+	cout << "Vector original: ";
+	for (int x : v) cout << x << " ";
+	cout << endl;
+
+	quicksort(v, 0, v.size() - 1);
+
+	cout << "\nVector ordenado (QuickSort): ";
+	for (int x : v) cout << x << " ";
+	cout << endl;
+
+	return 0;
+}
