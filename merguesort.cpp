febe79fa97679287f9451263e50866115baac8a9
diegougu <128000056+diegougu@users.noreply.github.com>
2025-11-18 12:59:29 -0500

    Create merguesort.cpp

---
+++ merguesort.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+void combinar(vector<int>& vc, int ini, int mid, int fin) {
+	vector<int> izquierda(vc.begin() + ini, vc.begin() + mid + 1);
+	vector<int> derecha(vc.begin() + mid + 1, vc.begin() + fin + 1);
+
+	int i = 0;
+	int j = 0; 
+	int k = ini;
+
+	while (i < izquierda.size() && j < derecha.size()) {
+		if (izquierda[i] <= derecha[j]) {
+			vc[k++] = izquierda[i++];
+		}
+		else {
+			vc[k++] = derecha[j++];
+		}
+	}
+	while (i < izquierda.size()) {
+		vc[k++] = izquierda[i++];
+	}
+
+	while (j < derecha.size()) {
+		vc[k++] = derecha[j++];
+	}
+
+}
+
+
+void merguersort(vector<int>& vc, int ini, int fin) {
+	if (ini >= fin) {
+		return;
+	}
+	int mid = (ini + fin) / 2;
+	merguersort(vc, ini, mid);
+	merguersort(vc, mid + 1, fin);
+	combinar(vc, ini, mid, fin);
+}
+
+int main() {
+	vector<int> v = { 9, 3, 7, 1, 6, 5, 2, 8, 4 };
+
+	cout << "Vector original: ";
+	for (int x : v) cout << x << " ";
+	cout << endl;
+
+	merguersort(v, 0, v.size() - 1);
+
+	cout << "\nVector ordenado (MergeSort): ";
+	for (int x : v) cout << x << " ";
+	cout << endl;
+
+	return 0;
+}
