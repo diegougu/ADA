e246f19994ace6b1ad5bf9ea3f770bbc28998136
diegougu <128000056+diegougu@users.noreply.github.com>
2025-11-22 18:14:46 -0500

    Update quicksort.cpp

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,35 +1,38 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-using namespace std;
 
+using namespace std;
 
 int pivot(vector<int>& vc, int ini, int fin) {
 	int pivotval = vc[ini];
 	int i = ini + 1;
 	int j = fin;
+
 	while (true) {
-		while (i <= fin && vc[i] <= pivotval) {
+		while (i <= fin && vc[i] <= pivotval) { // aca cambias el <= por >= y ya es decendente
 			i++;
 		}
-		while (j >= ini && vc[j] > pivotval) {
+
+		while (j >= ini && vc[j] > pivotval) { // aca cambias el > por < y ya es decendente
 			j--;
 		}
+
 		if (i >= j) {
 			break;
 		}
+
 		swap(vc[i], vc[j]);
 	}
-
 	swap(vc[ini], vc[j]);
 	return j;
-
 }
 
 void quicksort(vector<int>& vc, int ini, int fin) {
 	if (ini >= fin) {
 		return;
 	}
+
 	int pos = pivot(vc, ini, fin);
 	quicksort(vc, ini, pos - 1);
 	quicksort(vc, pos + 1, fin);
