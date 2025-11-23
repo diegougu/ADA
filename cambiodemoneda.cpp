4473bfc90c1feb76d1f3614f7c0d9176bb7b03f8
diegougu <128000056+diegougu@users.noreply.github.com>
2025-11-23 15:20:17 -0500

    Refactor cambiodemonedas function for dynamic denominations
    Refactor currency change calculation to use a dynamic list of denominations. Replace hardcoded map with a vector for flexibility.

--- cambiodemoneda.cpp
+++ cambiodemoneda.cpp
@@ -1,29 +1,30 @@
 #include <iostream>
-#include <map>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
+vector<int> cambiodemonedas(vector<int> monedas, int m) {
+    vector<int> solve;
+    sort(monedas.begin(), monedas.end(), greater<int>());
 
-map<int, int> mo = { {1,200},{2,100}, {3,50}, {4,20}, {5,10}, {6,5}, {7,2}, {8,1} };
-vector<int> cambio(int m) {
-	vector<int> vc;
-	while (m != 0) {
-		int r = 0;
-		for (auto e : mo) {
-			if (e.second > r && e.second <= m) {
-				r = e.second;
-			}
-		}
-		vc.push_back(r);
-		m -= r;
-	}
-	return vc;
+    while (m > 0) {
+        int restar = 0;
+        for (int i = 0; i < monedas.size(); i++) {
+            if (monedas[i] <= m && monedas[i] > restar) {
+                restar = monedas[i];
+            }
+        }
+        m = m - restar;
+        solve.push_back(restar);
+    }
+    return solve;
 }
 
 int main() {
-	vector<int> vc = cambio(389);
-	for (int i = 0; i < vc.size(); i++) {
-		cout << vc[i] << " ";
-	}
-	cout << endl;
+    vector<int> monedas = { 2, 50, 100, 20, 10, 5, 200 };
+    vector<int> solve = cambiodemonedas(monedas, 389); 
+
+    for (int i = 0; i < solve.size(); i++) {
+        cout << solve[i] << " ";
+    }
 }
