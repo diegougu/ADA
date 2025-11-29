265c9ec9ec9d11681399aeafcc3bb32cc8acee89
diegougu <128000056+diegougu@users.noreply.github.com>
2025-11-29 12:56:40 -0500

    Update merguesort.cpp

--- merguesort.cpp
+++ merguesort.cpp
@@ -3,13 +3,14 @@
 #include <algorithm>
 using namespace std;
 
-
 void combinar(vector<int>& vc, int ini, int mid, int fin) {
 	vector<int> izquierda(vc.begin() + ini, vc.begin() + mid + 1);
 	vector<int> derecha(vc.begin() + mid + 1, vc.begin() + fin + 1);
+
 	int i = 0;
 	int j = 0;
 	int k = ini;
+
 	while (i < izquierda.size() && j < derecha.size()) {
 		if (izquierda[i] <= derecha[j]) {
 			vc[k++] = izquierda[i++];
@@ -39,7 +40,7 @@ void merguesort(vector<int>& vc, int ini, int fin) {
 }
 
 int main() {
-	vector<int> vc = { 10,1,2,4,7,9,8,6,5,3 };
+	vector<int> vc = { 9,1,2,7,5,4,6,10,8,3 };
 	merguesort(vc, 0, vc.size() - 1);
 	for (int i = 0; i < vc.size(); i++) {
 		cout << vc[i] << " ";
