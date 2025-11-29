28a36bbb40b3315db6ba1636140695adee8d73be
diegougu <128000056+diegougu@users.noreply.github.com>
2025-11-29 13:27:56 -0500

    Refactor binary search function to busqueda

--- indiceIigualValor.cpp
+++ indiceIigualValor.cpp
@@ -1,30 +1,33 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-bool binarysearch(vector<int> vc, int ini, int fin, int oldmid) {
+bool busqueda(vector<int>& vc, int ini, int fin, int oldemid) {
 	if (ini == fin) {
 		return vc[ini] == ini;
 	}
+
 	int mid = (ini + fin) / 2;
+
+
+
 	if (vc[mid] == mid) {
 		return true;
 	}
-	else if (mid == oldmid) {
+	else if (mid == oldemid) {
 		return false;
 	}
-	else if (vc[mid] > mid) {
-		return binarysearch(vc, ini, mid - 1, mid);
+	else if (vc[ini] > ini) {
+		return busqueda(vc, ini, mid - 1, mid);
 	}
 	else {
-		return binarysearch(vc, mid + 1, fin, mid);
+		return busqueda(vc, mid + 1, fin, mid);
 	}
 }
 
 int main() {
-	vector<int> vc = { -3, -1, 2, 4, 5 };
-	cout << binarysearch(vc, 0, vc.size() - 1, 0) << endl;
+	vector<int> vc = { -3, -1, 1, 4, 5 };
+	cout << busqueda(vc, 0, vc.size() - 1, 0) << endl;
 
 }
