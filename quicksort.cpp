ef6c73b26bd384d34a24ba26129d0cf9b0489ce9
diegougu <128000056+diegougu@users.noreply.github.com>
2025-11-29 12:48:38 -0500

    Update quicksort.cpp

--- quicksort.cpp
+++ quicksort.cpp
@@ -9,6 +9,7 @@ int pivot(vector<int>& vc, int ini, int fin) {
 	int pivotval = vc[ini];
 	int i = ini + 1;
 	int j = fin;
+
 	while (true) {
 		while (i <= fin && vc[i] <= pivotval) {
 			i++;
@@ -21,6 +22,7 @@ int pivot(vector<int>& vc, int ini, int fin) {
 		if (i >= j) {
 			break;
 		}
+
 		swap(vc[i], vc[j]);
 	}
 
@@ -28,18 +30,18 @@ int pivot(vector<int>& vc, int ini, int fin) {
 	return j;
 }
 
-
 void quicksort(vector<int>& vc, int ini, int fin) {
 	if (ini >= fin) {
 		return;
 	}
+
 	int pos = pivot(vc, ini, fin);
 	quicksort(vc, ini, pos - 1);
 	quicksort(vc, pos + 1, fin);
 }
 
 int main() {
-	vector<int> vc = { 9,2,1,4,3,7,8,6,10,5 };
+	vector<int> vc = { 2,4,3,6,7,5,10,1,9,8 };
 	quicksort(vc, 0, vc.size() - 1);
 	for (int i = 0; i < vc.size(); i++) {
 		cout << vc[i] << " ";
